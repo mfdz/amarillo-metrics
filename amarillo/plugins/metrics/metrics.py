eee04a084093cfb86931659034397848abe333bd
Francia Csaba <cf@gerhardt.io>
2024-02-29 14:03:14 +0100

    Define secrets in the plugin

--- amarillo/plugins/metrics/metrics.py
+++ amarillo/plugins/metrics/metrics.py
@@ -15,7 +15,7 @@ from fastapi import Depends, HTTPException, FastAPI
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.responses import PlainTextResponse
 
-from amarillo.services.secrets import secrets
+from amarillo.plugins.metrics.secrets import secrets
 
 logger = logging.getLogger(__name__)
 
